aa7ad2435206986e370569f76a3cb3703b9abdf1
Андраник Вачаганович Григорян <53790991+MajorPayne36@users.noreply.github.com>
2021-05-03 13:19:49 +0300

    Create DBConnectionData.java

---
+++ src/main/java/com/Lesson1/JDBC/DBConnectionData.java
@@ -0,0 +1,23 @@
+package com.Lesson1.JDBC;
+
+public class DBConnectionData {
+    /**
+     * Database driver property name
+     */
+    public static final String DRIVER = "db.driver";
+
+    /**
+     * Database URL property name
+     */
+    public static final String URL = "db.url";
+
+    /**
+     * Database user property name
+     */
+    public static final String USER = "db.user";
+
+    /**
+     * User's password property name
+     */
+    public static final String PASSWORD = "db.password";
+}
